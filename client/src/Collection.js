b4acbeb522b0c29ad20f70aee85d6a73daf942a4
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-01-17 21:39:35 -0500

    Updated state of Collection component.

--- client/src/Collection.js
+++ client/src/Collection.js
@@ -17,6 +17,10 @@ class Collection extends Component {
       //method: 'text',
       domain: true,
       category: false,
+      newDomain: '',
+      newCategory: '',
+      addNewDomain: false,
+      addNewCategory: false,
       listOfDomains: [],
       listOfCategories: [],
       rawText: ''
@@ -32,6 +36,13 @@ class Collection extends Component {
   //     e.target.name: e.target.value
   //   });
   // };
+
+  handleNewEntry = (e) => {
+    this.setState({
+      addNewDomain: e.target.value === 'newDomain',
+      addNewCategory: e.target.value ==='newCategory'
+    });
+  }
 
   handleRecord = (e) => {
 
@@ -56,65 +67,73 @@ class Collection extends Component {
                 <Button color='primary' value='text' onClick={this.handleClick}>Text&ensp;<FaKeyboard /></Button>{' '}
                 <Button color='secondary' value='speech' onClick={this.handleClick}>Speech&ensp;<FaMicrophone /></Button>
               </FormGroup> */}
-              <FormGroup>
+              { !this.state.addNewDomain && <FormGroup>
                 <Label for='domain'>Domain</Label>
                 <Input type='select' name='domain' id='domain'>
                   { this.state.listOfDomains.length === 0 && <option>No domains created yet.</option> }
-                  { this.state.listOfDomains.map(d => (<option>{d}</option>)) }
+                  { this.state.listOfDomains.map(d => (<option value={d.value}>{d.label}</option>)) }
                 </Input>
-              </FormGroup>
+              </FormGroup> }
 
-              <FormGroup className={'text-center'}>
-                <Button color='primary' onClick={this.handleClick}>New Domain&ensp;<FaPlus /></Button>
-              </FormGroup>
+              { !this.state.addNewDomain && <FormGroup className={'text-center'}>
+                <Button color='primary' value='newDomain' onClick={this.handleNewEntry}>New Domain&ensp;<FaPlus /></Button>
+              </FormGroup> }
 
-              <FormGroup>
-                <InputGroup>
-                  <Input 
-                    id='newDomain'
-                    name='newDomain'
-                    value={this.newDomain} 
-                    placeholder='New domain, topic, data of interest...' />
-                  <InputGroupAddon addonType='append'>
-                    <Button color='danger'><FaMicrophoneAlt /></Button>
-                  </InputGroupAddon>
-                </InputGroup>
-              </FormGroup>
+              { this.state.addNewDomain && (
+                <div>
+                  <FormGroup>
+                    <Label for='newDomain'>New Domain</Label>
+                    <InputGroup>
+                      <Input 
+                        id='newDomain'
+                        name='newDomain'
+                        value={this.state.newDomain} 
+                        placeholder='New domain, topic, data of interest...' />
+                      <InputGroupAddon addonType='append'>
+                        <Button color='danger'><FaMicrophoneAlt /></Button>
+                      </InputGroupAddon>
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup className={'text-center'}>
+                    <Button color='success'>Create&ensp;<FaCheck /></Button>{' '}
+                    <Button color='danger'>Cancel&ensp;<FaTimes /></Button>
+                  </FormGroup>
+                </div>
+              ) }
 
-              <FormGroup className={'text-center'}>
-                <Button color='success'>Create&ensp;<FaCheck /></Button>{' '}
-                <Button color='danger'>Cancel&ensp;<FaTimes /></Button>
-              </FormGroup>
-
-              <FormGroup>
+              { !this.state.addNewCategory && <FormGroup>
                 <Label for='category'>Category</Label>
                 <Input type='select' name='category' id='category'>
                   { this.state.listOfCategories.length === 0 && <option>No categories created yet.</option> }
-                  { this.state.listOfCategories.map(d => <option>{d}</option>) }
-                </Input>
-              </FormGroup>
+                  { this.state.listOfCategories.map(c => <option value={c.value}>{c.label}</option>) }
+                </Input> 
+              </FormGroup> }
 
-              <FormGroup className={'text-center'}>
-                <Button color='primary' onClick={this.handleClick}>New Category&ensp;<FaPlus /></Button>
-              </FormGroup>
+              { !this.state.addNewCategory && <FormGroup className={'text-center'}>
+                <Button color='primary' value='newCategory' onClick={this.handleNewEntry}>New Category&ensp;<FaPlus /></Button>
+              </FormGroup> }
 
-              <FormGroup>
-                <InputGroup>
-                  <Input 
-                    id='newDomain'
-                    name='newDomain'
-                    value={this.newDomain} 
-                    placeholder='New category, classification, grouping...' />
-                  <InputGroupAddon addonType='append'>
-                    <Button color='danger'><FaMicrophoneAlt /></Button>
-                  </InputGroupAddon>
-                </InputGroup>
-              </FormGroup>
-
-              <FormGroup className={'text-center'}>
-                <Button color='success'>Create&ensp;<FaCheck /></Button>{' '}
-                <Button color='danger'>Cancel&ensp;<FaTimes /></Button>
-              </FormGroup>
+              { this.state.addNewCategory && (
+                <div>
+                  <FormGroup>
+                    <Label for='newCategory'>New Category</Label>
+                    <InputGroup>
+                      <Input 
+                        id='newCategory'
+                        name='newCategory'
+                        value={this.state.newCategory} 
+                        placeholder='New category, classification, grouping...' />
+                      <InputGroupAddon addonType='append'>
+                        <Button color='danger'><FaMicrophoneAlt /></Button>
+                      </InputGroupAddon>
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup className={'text-center'}>
+                    <Button color='success'>Create&ensp;<FaCheck /></Button>{' '}
+                    <Button color='danger'>Cancel&ensp;<FaTimes /></Button>
+                  </FormGroup>
+                </div>
+              ) }
 
               <FormGroup>
                 <Label for='rawText'>Data</Label>
@@ -127,12 +146,10 @@ class Collection extends Component {
               </FormGroup>
 
               <FormGroup className={'text-center'}>
-                <Button color='danger' onClick={this.handleRecord}>Record&ensp;<FaMicrophoneAlt /></Button>
+                <Button color='danger' onClick={this.handleRecord}>Record&ensp;<FaMicrophoneAlt /></Button>{' '}
+                <Button color='success' type='submit'>Submit&ensp;<FaPaperPlane /></Button>
               </FormGroup>
 
-              <FormGroup className={'text-center'}>
-                <Button color='success' type='submit'>Submit&ensp;<FaPaperPlane /></Button>
-              </FormGroup>                
             </Form>
           </Col>
         </Row>
