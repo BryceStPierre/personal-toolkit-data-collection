c8ce532c57c6b89652189cb7dbacdcbec164f9fd
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-02-02 18:33:18 -0500

    Updated timestamp in footer.

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container, Row, Col } from 'reactstrap';
 
@@ -6,27 +6,33 @@ import { FaHeart } from "react-icons/fa";
 
 import bryce from './bryce.png';
 
-const Footer = (props) => (
-  <Container className={'bg-light'}>
-    { props.user && <Row>
-      <Col className={'text-center mt-4'}>
-        <p className={'text-small mb-0'}>
-          Signed in as {props.user.displayNameShort}.<br/>Last login: {new Date(props.user.lastLogin).toLocaleString()}.
-        </p>
-      </Col>
-    </Row> }
-    <Row>
-      <Col className={'text-center mt-2 mb-2'}>
-        <img 
-          src={bryce} 
-          className={'branding-footer'} 
-          alt='Bryce St. Pierre' />
-        <p className={'text-center mt-2'}> 
-          Copyright &copy; {new Date().getFullYear()} Bryce St. Pierre.<br/>Made with <FaHeart className='text-red' /> in Ottawa.
-        </p>
-      </Col>
-    </Row>
-  </Container>
-);
+const Footer = (props) => {
+  let lastLogin = props.user ? new Date(props.user.lastLogin) : new Date();
+  
+  return (
+    <Container className={'bg-light'}>
+      { props.user && <Row>
+        <Col className={'text-center mt-4'}>
+          <p className={'text-small mb-0'}>
+            Signed in as {props.user.displayNameShort}.<br/>
+            Last login: {lastLogin.toDateString() + ' at ' + lastLogin.toLocaleTimeString() }.
+          </p>
+        </Col>
+      </Row> }
+      <Row>
+        <Col className={'text-center mt-2 mb-2'}>
+          <img 
+            src={bryce} 
+            className={'branding-footer'} 
+            alt='Bryce St. Pierre' />
+          <p className={'text-center mt-2'}> 
+            Copyright &copy; {new Date().getFullYear()} Bryce St. Pierre.<br/>
+            Made with <FaHeart className='text-red' /> in Ottawa.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Footer;
